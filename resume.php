77170b89be6e39993a6db84ad43d77fc5ee36228
Ron <me@ronaldarichardson.com>
2014-11-04 22:26:13 -0800

    resume styles and page

---
+++ resume.php
@@ -0,0 +1,243 @@
+<?php
+/*
+Template Name: Resume
+*/
+get_header(); ?>
+
+	<div id="content" class="site-content">
+
+		<div class="resume-content">
+			<div class="resume">
+				<div class="info">
+					<h2>Ronald A. Richardson</h2>
+					<h3 class="title">Software Engineer & UI/UX Designer</h3>
+					<p class="blurb">
+						I'm a coder with a passion for developing beautiful web applications that scale. I like to write clean code, and design solid UI/UX. I'm searching for work that engages and challenges me, and opportunities to explore new technologies.
+					</p>
+					<hr>
+					<h4 class="subject">Experience</h4>
+					<!-- freelance -->
+					<div class="pull-left">
+						<h3 class="title">Web Developer</h3>
+						<h3 class="sub-title">Freelance</h3>
+					</div>
+					<div class="pull-right">
+						<span class="date">Feb 2006 - Jun 2009</span>
+						<p>Charlotte, NC</p>
+					</div>
+					<div class="clearfix"></div>
+					<p>
+						Worked as a freelance developer with local businesses in Charlotte, NC while in High School. Designed, developed and deployed website and web applications for small businesses and local goverment. Some clients include the Mayor of Huntersville, renowned motivation speaker Tawana, and mega church Newbirth Charlotte. Development included Wordpress, PHP, and Javascript.
+					</p>
+					<!-- end freelance -->
+					<!-- usmcr -->
+					<div class="pull-left">
+						<h3 class="title">U.S. Marine</h3>
+						<h3 class="sub-title">USMCR</h3>
+					</div>
+					<div class="pull-right">
+						<span class="date">Aug 2008 - Present</span>
+						<p>North America</p>
+					</div>
+					<div class="clearfix"></div>
+					<p>
+						Serving in the United States Marine Corps Reserve honorably. During this time I have participated in several major training missions where I manned the combat operations center, tracking hundreds of Marines and convoys, provided radio support for medivac and supply, making sure each movement reached it's destination.
+					</p>
+					<!-- end usmcr -->
+					<!-- fsm -->
+					<div class="pull-left">
+						<h3 class="title">Software Developer</h3>
+						<h3 class="sub-title">Full Speed Marketing</h3>
+					</div>
+					<div class="pull-right">
+						<span class="date">Jul 2011 - Jan 2012</span>
+						<p>Tempe, AZ</p>
+					</div>
+					<div class="clearfix"></div>
+					<p>
+						Developed several applications for various companies, and in house apps. Some of the work includes iphone app, game development, large data import and transfer web application that utilized workers and several AWS EC2 instances to move data and report real time transfer progress, an open source spintax class in Python and PHP, and a desktop syntax highlighter progream for Spintax (a marketing language for creating semi unique content). I contributed my skills to the development of the Plivo PHP SDK, and ultimatley developed a call tracking application using both Twilio and Plivo API.
+					</p>
+					<!-- end fsm -->
+					<!-- fsm -->
+					<div class="pull-left">
+						<h3 class="title">Software Developer</h3>
+						<h3 class="sub-title">VUURR</h3>
+					</div>
+					<div class="pull-right">
+						<span class="date">Feb 2012 - Dec 2012</span>
+						<p>Chandler, AZ</p>
+					</div>
+					<div class="clearfix"></div>
+					<p>
+						Developed several applications for various companies, some companies include Infusionsoft,  and Shamrock Farms. I worked as sole developer for many projects including web based dashboard to report analytical data, facebook apps, and wordpress development. Used several API's such as Google, Facebook, Sendible, Twitter, and Authority Labs. Rewrote the official Infusionsoft Python SDK from over 3k lines of code into a <a href="http://goo.gl/k9ZbJ8">light 27 line code library</a>.
+					</p>
+					<!-- end fsm -->
+					<!-- tc -->
+					<div class="pull-left">
+						<h3 class="title">Software Engineer</h3>
+						<h3 class="sub-title">Tango Card, Inc</h3>
+					</div>
+					<div class="pull-right">
+						<span class="date">Feb 2012 - Dec 2012</span>
+						<p>Seattle, WA</p>
+					</div>
+					<div class="clearfix"></div>
+					<p>
+						Wrote essential applications, maintained current codebase, and solved complex issues. While at Tango Card I developed there entire back-end content management system that uses a RESTful interface to feed updated pages, layout styles to the outdated Zend Framework based front-end application. The CMS is fully featured including it's own S3 Manager for assets, a syntax highlighter and handlebar styled template system built in Grails. Contributed a majority of the effort to redesign and revampe the entire front-end website from PSD to production. Designed and developed the front-end and parts of the backe-end component of Tango Card's star platform "Rewards Genius" which now serves many major companies including Twitter, Citrux, Salesforce, and more.
+					</p>
+					<!-- end tc -->
+					<hr>
+					<h4 class="subject">Education</h4>
+					<!-- uat -->
+					<div class="pull-left">
+						<h3 class="title">Bachelors in Robotics and Embedded Systems</h3>
+						<h3 class="sub-title">University of Advancing Technology</h3>
+					</div>
+					<div class="pull-right">
+						<span class="date">2010</span>
+						<p>Tempe, AZ</p>
+					</div>
+					<div class="clearfix"></div>
+					<p>
+						Studied Robotics and Embedded systems, worked with microcontrollers and other technologies, designed schematics and real world use of robotics. I was also awarded the Ray Kurzweil scholarship for the development of an open source web based OS titled "ZocialNET".
+					</p>
+					<!-- end uat -->
+				</div>
+				<div class="skills">
+					<h4 class="subject">Get in Touch</h4>
+					<ul class="getintouch">
+						<li>
+							<span><i class="fa fa-paper-plane-o"></i></span>
+							<a href="mailto:me@ronaldarichardson.com">me@ronaldarichardson.com</a>
+						</li>
+						<li>
+							<span><i class="fa fa-phone"></i></span>
+							(480) 621-2748
+						</li>
+						<li>
+							<span><i class="fa fa-desktop"></i></span>
+							<a href="http://www.iamronald.com">www.iamronald.com</a>
+						</li>
+						<li>
+							<span><i class="fa fa-github-alt"></i></span>
+							<a href="http://www.github.com/theprestig3">github.com/theprestig3</a>
+						</li>
+					</ul>
+					<hr>
+					<h4 class="subject">Skills</h4>
+					<!-- php -->
+					<h3 class="sub-title">PHP</h3>
+					<div class="progress">
+						<div class="progress-bar progress-bar-success progress-bar-striped active"  role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+							<span class="sr-only">100% Competence</span>
+						</div>
+					</div>
+					<p class="sblurb">Design and develop object oriented applications, RESTful interfaces, use extensions, composer, and setup LAMP</p>
+					<!-- php -->
+					<!-- java -->
+					<h3 class="sub-title">Java</h3>
+					<div class="progress">
+						<div class="progress-bar progress-bar-success progress-bar-striped active"  role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%">
+							<span class="sr-only">70% Competence</span>
+						</div>
+					</div>
+					<p class="sblurb">Design and develop object oriented applications, RESTful interfaces, and desktop applications</p>
+					<!-- java -->
+					<!-- grails -->
+					<h3 class="sub-title">Groovy/ Grails</h3>
+					<div class="progress">
+						<div class="progress-bar progress-bar-success progress-bar-striped active"  role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+							<span class="sr-only">80% Competence</span>
+						</div>
+					</div>
+					<p class="sblurb">Design and develop web applications, and RESTful interfaces</p>
+					<!-- grails -->
+					<!-- python -->
+					<h3 class="sub-title">Python</h3>
+					<div class="progress">
+						<div class="progress-bar progress-bar-success progress-bar-striped active"  role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+							<span class="sr-only">80% Competence</span>
+						</div>
+					</div>
+					<p class="sblurb">Design and develop web applications, RESTful interfaces, desktop applications, and games</p>
+					<!-- python -->
+					<!-- js -->
+					<h3 class="sub-title">Javascript/ jQuery/ AJAX</h3>
+					<div class="progress">
+						<div class="progress-bar progress-bar-success progress-bar-striped active"  role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+							<span class="sr-only">100% Competence</span>
+						</div>
+					</div>
+					<p class="sblurb">Design and develop object oriented applications/ tools, and cool client side interaction</p>
+					<!-- js -->
+					<!-- CSS -->
+					<h3 class="sub-title">CSS/ CSS3/ SASS</h3>
+					<div class="progress">
+						<div class="progress-bar progress-bar-success progress-bar-striped active"  role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+							<span class="sr-only">100% Competence</span>
+						</div>
+					</div>
+					<p class="sblurb">Design and style intuitive UI/ UX for websites and web apps</p>
+					<!-- CSS -->
+					<!-- sql -->
+					<h3 class="sub-title">MySQL/ SQL</h3>
+					<div class="progress">
+						<div class="progress-bar progress-bar-success progress-bar-striped active"  role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+							<span class="sr-only">100% Competence</span>
+						</div>
+					</div>
+					<p class="sblurb">Design solid scaling schema, write optomied querys and use SQL to the full extent of it's abilities</p>
+					<!-- sql -->
+					<!-- ut -->
+					<h3 class="sub-title">Unit Testing</h3>
+					<div class="progress">
+						<div class="progress-bar progress-bar-success progress-bar-striped active"  role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%">
+							<span class="sr-only">70% Competence</span>
+						</div>
+					</div>
+					<p class="sblurb">Write solid unit test for applications, and UI/UX testing using Nightmare and Selenium</p>
+					<!-- ut -->
+					<!-- mobile -->
+					<h3 class="sub-title">iOS Objective-C, Android Dev</h3>
+					<div class="progress">
+						<div class="progress-bar progress-bar-success progress-bar-striped active"  role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%">
+							<span class="sr-only">50% Competence</span>
+						</div>
+					</div>
+					<p class="sblurb">Design and write simple mobile applications</p>
+					<!-- mobile -->
+					<!-- flash -->
+					<h3 class="sub-title">Flash/ Brightscript</h3>
+					<div class="progress">
+						<div class="progress-bar progress-bar-success progress-bar-striped active"  role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+							<span class="sr-only">60% Competence</span>
+						</div>
+					</div>
+					<p class="sblurb">Design and develop flash based components and applications, and develop full featured Roku apps</p>
+					<!-- flash -->
+					<!-- html5 -->
+					<h3 class="sub-title">HTML5</h3>
+					<div class="progress">
+						<div class="progress-bar progress-bar-success progress-bar-striped active"  role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+							<span class="sr-only">40% Competence</span>
+						</div>
+					</div>
+					<p class="sblurb">Utilize the canvas to hanlde processes that would usually be done server side (for example HTML to Image)</p>
+					<!-- html5 -->
+					<!-- ps -->
+					<h3 class="sub-title">Photoshop</h3>
+					<div class="progress">
+						<div class="progress-bar progress-bar-success progress-bar-striped active"  role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+							<span class="sr-only">60% Competence</span>
+						</div>
+					</div>
+					<p class="sblurb">I can design logos, manipulate photos and images, convert PSD to HTML/CSS and create UI/UX mockups</p>
+					<!-- ps -->
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+
+	</div><!-- #content -->
+
+<?php get_footer(); ?>

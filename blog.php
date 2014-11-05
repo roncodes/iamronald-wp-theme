032d39bcbde660d95a819d4b0f3c4705fb1ca6fd
Ron <me@ronaldarichardson.com>
2014-11-04 22:55:26 -0800

    blog page update

--- blog.php
+++ blog.php
@@ -2,6 +2,15 @@
 /*
 Template Name: Blog
 */
+$current_page = (get_query_var('paged')) ? get_query_var('paged') : 1; // get current page number
+$args = array(
+	'posts_per_page' => get_option('posts_per_page'), // the value from Settings > Reading by default
+	'paged'          => $current_page // current page
+);
+query_posts( $args );
+ 
+$wp_query->is_archive = true;
+$wp_query->is_home = false;
 
 get_header(); ?>
 
